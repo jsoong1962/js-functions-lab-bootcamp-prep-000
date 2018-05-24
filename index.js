912e3c9ed099d805185e818d561505004a3cde2d
Johnny Soong <johnnysoong@hotmail.com>
2018-05-24 19:27:55 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 // write your code below!
 function happyHolidays() {
-  return "Happy holidays!"
+  return "Happy holidays!";
 }
 
+function happyHolidaysTo(name) {
+  return `Happy holidays to ${name}!`;
+}
